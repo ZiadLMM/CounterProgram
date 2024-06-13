f9d83ff85ff5a0c98ad4507ed4207d16cf73ab9a
ZiadLMM <161867781+ZiadLMM@users.noreply.github.com>
2024-06-13 14:10:42 +0300

    Add files via upload

---
+++ CounterProgram/index.js
@@ -0,0 +1,21 @@
+// counter program
+
+const decreaseBtn = document.getElementById("decreaseBtn");
+const resetBtn = document.getElementById("resetBtn");
+const increaseBtn = document.getElementById("increaseBtn");
+const countLabel = document.getElementById("countlabel");
+let count = 0;
+
+increaseBtn.onclick = function(){
+    count++;
+    countlabel.textContent = count;
+}
+decreaseBtn.onclick = function(){
+    count--;
+    countlabel.textContent = count;
+}
+
+resetBtn.onclick = function(){
+    count = 0;
+    countlabel.textContent = count;
+}
